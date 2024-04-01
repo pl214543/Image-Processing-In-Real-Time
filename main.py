f8d37ce0367b307737e197d500daaa6bcb94c217
Alexey Guydouk <144239447+pl214543@users.noreply.github.com>
2024-04-01 18:51:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,96 +1,57 @@
-# LINES NOTATED
+#for when entering the password. hashing will encrypt the password in the sql database
+import hashlib
 
-# import required libraries
-import cv2
-import numpy as num
+#for file paths
+import os
 
-# import functions
-from reader import rectangle, crop
-from optimization import optimize
-from lineDraw import contours
+#imports sqlite3, which will be used to create a sql database
 
-# video capture
-video = cv2.VideoCapture("Timeline+1.mov")
+#imports all of the required tkinter libraries
+from tkinter import *
+root = Tk()
+from tkinter import ttk
+from tkinter import messagebox
+import tkinter as tk
 
-# test case
-print(video.isOpened())
+from datetime import datetime
 
-# check if the video is opened
-while video.isOpened():
+import sqlpage
 
-    # gets the frame from the video to draw on
-    booleanReady, frame = video.read()
+# creates the tk gui
+root.geometry()
+root.geometry("510x1020")
+root.title("Choose")
 
-    # retrieves the height and width of the frame for masking
-    height, width = frame.shape[:2]
+#function that will open up the sign in page
+def openSignUpPage():
+  root.destroy()
+  time = datetime.now()
+  current_time = time.strftime("%Y-%m-%d %H:%M:%S %Z%z")
+  with open('logText.txt', 'at') as f:
+    f.write("Signup page opened at " + current_time + "\n")
+    f.close()
+  import signup
 
-    pt_A = [-300, 600]
-    pt_B = [-300, 900]
-    pt_C = [-1080, 600]
-    pt_D = [-1080, 900]
 
-    # Here, I have used L2 norm. You can use L1 also.
-    width_AD = num.sqrt(((pt_A[0] - pt_D[0]) ** 2) + ((pt_A[1] - pt_D[1]) ** 2))
-    width_BC = num.sqrt(((pt_B[0] - pt_C[0]) ** 2) + ((pt_B[1] - pt_C[1]) ** 2))
-    maxWidth = max(int(width_AD), int(width_BC))
+#function that will open up the log in page
+def openLogInPage():
+  root.destroy()
+  time = datetime.now()
+  current_time = time.strftime("%Y-%m-%d %H:%M:%S %Z%z")
+  with open('logText.txt', 'at') as f:
+    f.write("Login page opened at " + current_time + "\n")
+    f.close()
+  import login
 
-    height_AB = num.sqrt(((pt_A[0] - pt_B[0]) ** 2) + ((pt_A[1] - pt_B[1]) ** 2))
-    height_CD = num.sqrt(((pt_C[0] - pt_D[0]) ** 2) + ((pt_C[1] - pt_D[1]) ** 2))
-    maxHeight = max(int(height_AB), int(height_CD))
+#creates a login button and sets up their location. The login button runs a function to open up the login screen.
+login_button = Button(root, text="Login", command=openLogInPage)
+login_button.grid(row=0,column=0)
+#creates a signup button and sets up their location. The signup button runs a function to open up the signup screen.
+signup_button = Button(root, text="Create Account", command=openSignUpPage)
+signup_button.grid(row=1,column=0)
+#creates an exit button that will destroy the gui. This will end the program.
+enter_button = Button(root, text="Exit", command=root.destroy)
+enter_button.grid(row=2,column=0)
 
-    # Locate points of the documents
-    # or object which you want to transform
-
-    inputpoints = num.float32([pt_A, pt_B, pt_C, pt_D])
-    outputpoints = num.float32([[0, 0],
-                             [0, maxHeight - 1],
-                             [maxWidth - 1, maxHeight - 1],
-                             [maxWidth - 1, 0]])
-
-# https://theailearner.com/tag/cv2-getperspectivetransform/
-
-    # Apply Perspective Transform Algorithm
-    matrix = cv2.getPerspectiveTransform(inputpoints, outputpoints)
-    result = cv2.warpPerspective(frame, matrix, (500, 600))
-
-    # # creates a rectangle with background of 0s for masking
-    # zerosRectangle = num.zeros((height, width), dtype="uint8")
-    #
-    # # defines coordinates for masking as percentage of camera view so compatible with any camera
-    # topLeftX = int(width * 0.175)
-    # topLeftY = int(height * 0.725)
-    # bottomRightX = int(width * 0.85)
-    # bottomRightY = int(height * 0.25)
-    #
-    # # creates list of those coordinates for drawing the rectangles and masking later, easy to use as parameters
-    # varList = [topLeftX, topLeftY, bottomRightX, bottomRightY]
-
-    # grayscale
-    # grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # uses the functions from the other files to optimize, make a rectangle, and crop
-    # optimize will employ Gaussian Blur and Canny Edge Detection
-    # optimized = optimize(grey)
-
-    # cropped will create the mask
-    # cropped = crop(optimized, zerosRectangle, varList)
-
-    # calls the contours function to detect contours and draw
-    # contoured = contours(cropped, frame)
-
-    # addRectangle will draw the rectangle around the mask. put after others so contours doesn't detect the rectangle
-    # addRectangle = rectangle(contoured, varList)
-
-    # display the frame, creating a video
-    cv2.imshow('Frame', frame)
-    cv2.imshow('Transform', result)
-
-    # closes the window when 'q' is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# releases the video capture
-video.release()
-
-# closes the video
-cv2.destroyAllWindows()
+# keeps the gui from disappearing/keeps it on screen
+root.mainloop()
