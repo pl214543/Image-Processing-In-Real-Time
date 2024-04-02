d782f9b99cf9dde327a2a941a7ae38e7b18e12ff
Alexey Guydouk <144239447+pl214543@users.noreply.github.com>
2024-04-01 21:52:21 -0400

    Update perspectivetransform.py

--- perspectivetransform.py
+++ perspectivetransform.py
@@ -2,17 +2,21 @@
 import cv2
 import numpy as np
 
+from optimization import optimize
+from lineDraw import drawing
+from haardetect import haar_detect1, haar_detect2
+
+arrow = cv2.imread('kjhfd.png')
+
 def perspective_transform(frame):
+    
+    # coordinates for perspective transform
     bl = [173, 346]
     tl = [266, 276]
     tr = [486, 276]
     br = [686, 341]
 
-    cv2.circle(frame, bl, 5, (0, 0, 255), -1)
-    cv2.circle(frame, tl, 5, (0, 0, 255), -1)
-    cv2.circle(frame, br, 5, (0, 0, 255), -1)
-    cv2.circle(frame, tr, 5, (0, 0, 255), -1)
-
+    # puts all coords in a list
     pts1 = np.float32([tl, bl, tr, br])
     pts2 = np.float32([[0, 0], [0, 480], [852, 0], [852, 480]])
 
@@ -22,6 +26,24 @@ def perspective_transform(frame):
     secmatrix = cv2.getPerspectiveTransform(pts2, pts1)
     warped = cv2.warpPerspective(result, secmatrix, (852, 480))
 
+    # grayscale
+    grey = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
+
+    # uses the functions from the other files to optimize, make a rectangle, and crop
+    # optimize will employ Gaussian Blur and Canny Edge Detection
+    optimized = optimize(grey)
+
+    # warped = haar_detect1(warped)
+    # warped = haar_detect2(warped)
+
+    # detect the lines using HoughLines
+    lines = cv2.HoughLinesP(optimized, 0.5, np.pi / 180, 15, None, 40, 500)
+
+    # draw the final lines
+    result = drawing(warped, lines)
+
     final = cv2.addWeighted(frame, 1, warped, 0.7, 0)
+
+    final = cv2.add(final, arrow)
 
     return final
